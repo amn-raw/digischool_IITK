1a2cb7dc3c9a658893f025e7f33fad87b9cac2ce
Aman Raw <79910428+amn-raw@users.noreply.github.com>
2022-08-22 23:57:22 +0530

    Add files via upload

---
+++ profileapp/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from profileapp.models import USER_PROFILE_DATABASE
+# Register your models here.
+admin.site.register(USER_PROFILE_DATABASE)
---
+++ testapp/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from testapp.models import ALL_TESTS
+# Register your models here.
+admin.site.register(ALL_TESTS)
